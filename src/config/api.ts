02402bbad88c798d8983096c1fd8d2091fdd93f2
mrshahriar <toshahriarshakil@gmail.com>
2023-04-02 02:50:07 +0600

    update readme

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 export const request = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  // baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
+  baseURL: "https://dummyjson.com/",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
